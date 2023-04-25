636a4927f3908a402e8c0984364e2e4c649d0960
Krist Ponpairin <krist7599555@gmail.com>
2023-04-26 03:04:20 +0700

    fix: prod domain is github.io

--- docs/config.mjs
+++ docs/config.mjs
@@ -1,6 +1,4 @@
-export const base_path = window.location.href.includes(
-  "krist7599555.github.com"
-)
+export const base_path = window.location.href.includes("krist7599555.github.io")
   ? "/example-vanilla-cash-dom"
   : "";
 console.log("base_path:", base_path);
