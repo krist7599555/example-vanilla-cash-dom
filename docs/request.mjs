33640adc1f74c3a191b597e1815d07a281ab932c
Krist Ponpairin <krist7599555@gmail.com>
2023-04-26 02:55:17 +0700

    fix: base_path prod

--- docs/request.mjs
+++ docs/request.mjs
@@ -1,10 +1,11 @@
 // @ts-check
+import { base_path } from "./config.mjs";
 
 /**
  * @returns {Promise<{ data: { id: string, name: string }[] }>}
  */
 export function request_api_users_$get() {
-  return fetch("/data_users.json").then((res) => res.json());
+  return fetch(`${base_path}/data_users.json`).then((res) => res.json());
   return fetch("/api/users").then((res) => res.json());
 }
 
@@ -12,6 +13,6 @@ export function request_api_users_$get() {
  * @returns {Promise<{ data: { id: string, startdate: string, name: string, participate: string[] }[] }>}
  */
 export function request_api_events_$get() {
-  return fetch("/data_events.json").then((res) => res.json());
+  return fetch(`${base_path}/data_events.json`).then((res) => res.json());
   return fetch("/api/events").then((res) => res.json());
 }
